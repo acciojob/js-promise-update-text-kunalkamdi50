d57e3aa524ba4dacb85e5099808f645934cdd020
kunalkamdi50 <128037794+kunalkamdi50@users.noreply.github.com>
2023-05-12 20:47:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 //your JS code here. If required.
-function delayMessage() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve("Hello, world!");
-    }, 1000);
-  });
+// function delayMessage() {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve("Hello, world!");
+//     }, 1000);
+//   });
+// }
+
+// // Update HTML element once promise resolves
+// delayMessage()
+//   .then((message) => {
+//     const outputElement = document.getElementById("output");
+//     outputElement.textContent = message;
+//   })
+//   .catch((error) => {
+//     console.log("Error:", error);
+//   });
+
+const promise = new Promise((resolve, reject) =>{ 
+  setTimeout(()=>{
+    resolve("Hello, World!")
+  } ,1000)
+})
+
+const showMassege = (message) => {
+  const output = document.getElementById("output")
+  output.innerText = message;
 }
 
-// Update HTML element once promise resolves
-delayMessage()
-  .then((message) => {
-    const outputElement = document.getElementById("output");
-    outputElement.textContent = message;
-  })
-  .catch((error) => {
-    console.log("Error:", error);
-  });
-
+promise.then(showMassege);
