5b8e04322bc28222cbf650e620695279be546d71
kunalkamdi50 <128037794+kunalkamdi50@users.noreply.github.com>
2023-05-12 20:30:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+function delayMessage() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000);
+  });
+}
+
+// Update HTML element once promise resolves
+delayMessage()
+  .then((message) => {
+    const outputElement = document.getElementById("output");
+    outputElement.textContent = message;
+  })
+  .catch((error) => {
+    console.log("Error:", error);
+  });
+
